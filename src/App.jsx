02fabf671b13fc1c6d1aa1d0a1f8ed78d5e5ab1a
mrwindupbirdesq <nmaahs11@gmail.com>
2024-02-18 16:11:53 -0800

    Test db

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import './App.css';
 import { db } from './firebase-config';
+import { set, ref } from 'firebase/database';
 
 function App() {
-  const writeToDB = () => {};
-  return <></>;
+  const writeToDB = () => {
+    set(ref(db, 'test'), {
+      menu: 'butternut'
+    });
+  };
+
+  return (
+    <>
+      <input type="text" />
+      <button onClick={writeToDB}>submit</button>
+    </>
+  );
 }
 
 export default App;
