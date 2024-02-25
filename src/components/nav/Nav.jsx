ecfacda6df36c2d888e395b9c583bae82b60f3fb
mrwindupbirdesq <nmaahs11@gmail.com>
2024-02-25 15:02:00 -0800

    Add sign out button

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,13 +1,18 @@
 import { Link } from 'react-router-dom';
 import styles from './nav.module.css';
+import { useAuth } from '../AuthContext';
 
 export default function Nav() {
+  const { signOut } = useAuth();
   return (
     <div className={styles.navBar}>
       <h1 className={styles.title}>Wild Roots Kitchen Vashon: Admin Page</h1>
       <Link to="/" className={styles.link}>
         Home
       </Link>
+      <button className={styles.signOut} onClick={signOut}>
+        Sign Out
+      </button>
     </div>
   );
 }
