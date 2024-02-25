c0ddaa0a680cba1dc5fd76d9d509b542587ad29b
mrwindupbirdesq <nmaahs11@gmail.com>
2024-02-24 16:15:40 -0800

    Extract menu updater

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,13 @@
 import './App.css';
 import { useEffect, useState } from 'react';
-import { db } from './firebase-config';
-import { set, ref, onValue, remove, update } from 'firebase/database';
+import Menu from './components/menu/Menu';
 
 function App() {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    onValue(ref(db), (snapshot) => {
-      const dataRead = snapshot.val();
-      console.log(dataRead);
-      if (dataRead != null) {
-        setData(dataRead);
-      }
-    });
-  }, []);
-
-  const writeToDB = (e) => {
-    e.preventDefault();
-    set(ref(db, 'test'), {
-      menu: 'buttet'
-    });
-  };
-
-  const handleDelete = () => {
-    remove(ref(db, 'test'));
-  };
-
-  const handleEdit = () => {
-    update(ref(db, 'test'), {
-      menu: 'butternut foo'
-    });
-  };
-
+  //if user auth, show dash with buttons linking to various edit options
+  // else show login/ auth option
   return (
     <>
-      <input type="text" />
-      <button onClick={writeToDB}>submit</button>
-      <h1>{data && data.test.menu}</h1>
-      <button onClick={handleDelete}>delete</button>
-      <button onClick={handleEdit}>edit</button>
+      <Menu />
     </>
   );
 }
---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,131 @@
+import styles from './menu.module.css';
+import { useEffect, useState } from 'react';
+import { db } from '../../firebase-config';
+import { set, ref, onValue, remove, update } from 'firebase/database';
+
+function Menu() {
+  const [sections, setSections] = useState(null);
+  const [items, setItems] = useState(null);
+
+  useEffect(() => {
+    onValue(ref(db), (snapshot) => {
+      const data = snapshot.val();
+      if (data.menu.sections != null) {
+        setSections(data.menu.sections);
+      }
+      if (data.menu.items != null) {
+        setItems(data.menu.items);
+      }
+    });
+  }, []);
+
+  const handleChange = (e, index, type) => {
+    const updatedSections = [...sections];
+    const updatedItems = [...items];
+
+    if (type === 'section') {
+      if (e.target.name == 'sectionTitle') {
+        const prevSectionTitle = updatedSections[index].sectionTitle;
+        //update itemSections for items, so that section retains its items
+        updatedItems.forEach((item) => {
+          if (item.itemSection == prevSectionTitle) {
+            item.itemSection = e.target.value;
+          }
+        });
+        setItems(updatedItems);
+      }
+      updatedSections[index][e.target.name] = e.target.value;
+      setSections(updatedSections);
+    } else if (type === 'item') {
+      updatedItems[index][e.target.name] = e.target.value;
+      setItems(updatedItems);
+    }
+  };
+
+  const writeToDB = (e) => {
+    e.preventDefault();
+    set(ref(db, 'menu'), {
+      sections: sections,
+      items: items
+    });
+  };
+
+  const resetMenu = () => {
+    set(ref(db, 'menu'), {
+      sections: [{ sectionTitle: '', sectionDetail: '' }],
+      items: [
+        { itemTitle: '', itemDescription: '', itemSection: itemSectionTitle }
+      ]
+    });
+  };
+
+  const addMenuItem = (itemSectionTitle) => {
+    setItems([
+      ...items,
+      { itemTitle: '', itemDescription: '', itemSection: itemSectionTitle }
+    ]);
+  };
+
+  const addMenuSection = () => {
+    setSections([...sections, { sectionTitle: '', sectionDetail: '' }]);
+  };
+
+  return (
+    <div className={styles.menuContainer}>
+      <button onClick={writeToDB}>Submit Menu</button>
+      <button onClick={resetMenu}>Clear Menu</button>
+      {sections &&
+        sections.map((section, sectionIndex) => (
+          <div className={styles.sectionContainer} key={sectionIndex}>
+            <label htmlFor="">Section Title</label>
+            <textarea
+              value={section.sectionTitle}
+              onChange={(e) => handleChange(e, sectionIndex, 'section')}
+              name="sectionTitle"
+              cols="30"
+              rows="10"
+            ></textarea>
+            <label htmlFor="">Section Detail</label>
+            <textarea
+              value={section.sectionDetail}
+              onChange={(e) => handleChange(e, sectionIndex, 'section')}
+              name="sectionDetail"
+              cols="30"
+              rows="10"
+            ></textarea>
+            {items &&
+              items.map((item, itemIndex) => {
+                if (item.itemSection === section.sectionTitle) {
+                  return (
+                    <div className={styles.itemContainer} key={itemIndex}>
+                      <label htmlFor="">Item Title</label>
+                      <textarea
+                        value={item.itemTitle}
+                        onChange={(e) => handleChange(e, itemIndex, 'item')}
+                        name="itemTitle"
+                        cols="30"
+                        rows="10"
+                      ></textarea>
+                      <label htmlFor="">Item Description</label>
+                      <textarea
+                        value={item.itemDescription}
+                        onChange={(e) => handleChange(e, itemIndex, 'item')}
+                        name="itemDescription"
+                        cols="30"
+                        rows="10"
+                      ></textarea>
+                    </div>
+                  );
+                }
+              })}
+            <button onClick={() => addMenuItem(section.sectionTitle)}>
+              Add item
+            </button>
+          </div>
+        ))}
+      <button onClick={addMenuSection}>Add section</button>
+    </div>
+  );
+}
+
+export default Menu;
