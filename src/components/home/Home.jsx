3953af9cad645edc754253af839f9ce6f7695272
mrwindupbirdesq <nmaahs11@gmail.com>
2024-02-29 11:15:12 -0800

    Add basic stylings to home page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,12 +1,18 @@
 import { Link } from 'react-router-dom';
+import styles from './home.module.css';
 
 export default function Home() {
   return (
-    <>
-      <h3>Edit Options</h3>
-      <ul>
-        <Link to="/menu">Edit Menu</Link>
-      </ul>
-    </>
+    <div className={styles.container}>
+      <p>Welcome!</p>
+      <p>To make edits to the weekly menu, click the edit menu button.</p>
+      <Link className={styles.linkBtn} to="/menu">
+        Edit Menu
+      </Link>
+      <p>To edit other areas of the site, click edit general info</p>
+      <Link className={styles.linkBtn} to="/info">
+        Edit General Info
+      </Link>
+    </div>
   );
 }
