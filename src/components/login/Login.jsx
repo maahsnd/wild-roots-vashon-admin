c0a27f8c00b783044bd5b28be0d4aac213f033c8
mrwindupbirdesq <nmaahs11@gmail.com>
2024-03-09 13:52:46 -0800

    Refactor Enable form submission on enter. Display loading state while signing in.

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { useAuth } from '../AuthContext'; // Import the AuthContext
 import {
   getAuth,
@@ -12,9 +12,12 @@ const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState(null); // State variable for error message
+  const [loading, setLoading] = useState(false); // State variable for loading state
+  const formRef = useRef(null);
 
   const handleSignIn = async (e) => {
     e.preventDefault(); // Prevent default form submission behavior
+    setLoading(true); // Set loading state to true
     try {
       const auth = getAuth();
       // Set persistence to session
@@ -27,13 +30,15 @@ const Login = () => {
       // Handle sign-in error
       console.error('Sign-in error:', error.message);
       setError(error.message); // Set error message in state
+    } finally {
+      setLoading(false); // Set loading state to false
     }
   };
 
   return (
     <div>
       <h2>Sign In</h2>
-      <form onSubmit={handleSignIn}>
+      <form onSubmit={handleSignIn} ref={formRef}>
         <input
           type="email"
           placeholder="Email"
@@ -50,7 +55,9 @@ const Login = () => {
           autoComplete="current-password"
           id="password"
         />
-        <button type="submit">Sign In</button>
+        <button type="submit" name="submit" disabled={loading}>
+          {loading ? 'Signing In...' : 'Sign In'}
+        </button>
       </form>
       {error && <p style={{ color: 'red' }}>{error}</p>}
     </div>
