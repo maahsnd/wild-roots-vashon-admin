15e8f1753dce548900b5e6e1b44965ef98d4af74
mrwindupbirdesq <nmaahs11@gmail.com>
2024-03-09 16:28:36 -0800

    Format component

--- src/components/general-photos/GeneralPhotos.jsx
+++ src/components/general-photos/GeneralPhotos.jsx
@@ -1,20 +1,29 @@
-import {  useState } from 'react';
+import { useState } from 'react';
 import styles from './general_photos.module.css';
 import PhotoUploader from '../photo-uploader/PhotoUploader';
 
 function GeneralPhotos() {
-  const [pageName, setPageName] = useState(null)
-
+  const [pageName, setPageName] = useState(null);
 
   return (
     <div className={styles.container}>
       <div className={styles.pageSelectButtons}>
         <h3>Select page to edit photos: </h3>
-      <button onClick={()=> setPageName('aboutPhotos')} className={styles.pageBtn}>About</button>
-      <button onClick={()=> setPageName('farmPhotos')} className={styles.pageBtn}>Farm</button>
+        <button
+          onClick={() => setPageName('aboutPhotos')}
+          className={styles.pageBtn}
+        >
+          About
+        </button>
+        <button
+          onClick={() => setPageName('farmPhotos')}
+          className={styles.pageBtn}
+        >
+          Farm
+        </button>
       </div>
-    {pageName && <p>Edit {pageName}</p>}
-      {pageName && <PhotoUploader folderName={pageName}/>}
+      {pageName && <p>Edit {pageName}</p>}
+      {pageName && <PhotoUploader folderName={pageName} />}
     </div>
   );
 }
