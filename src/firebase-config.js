7f4c8d97ec6c98548cf2a035cbcbdeed200a72d7
mrwindupbirdesq <nmaahs11@gmail.com>
2024-04-04 16:02:13 -0700

    Add db url

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -6,6 +6,7 @@ import { getStorage } from 'firebase/storage';
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASEURL,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
